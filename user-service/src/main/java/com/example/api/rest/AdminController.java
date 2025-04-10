d04c6f67b4a1936ef48d5e41fefc75ef12682577
Rodion Rybko <rybkorodion201@mail.ru>
2025-04-11 00:11:01 +0700

    create getaway service

--- user-service/src/main/java/com/example/api/rest/AdminController.java
+++ user-service/src/main/java/com/example/api/rest/AdminController.java
@@ -17,31 +17,26 @@ public class AdminController {
 
     private final AdminService adminService;
 
-    //admin?
     @GetMapping(ApiPaths.USERS_LIST)
     public List<UserProfileDto> UsersProfiles(@RequestParam List<UUID> usersId, @RequestHeader("Authorization") String authHeader){
         return adminService.getUsersList(usersId, authHeader.substring(7));
     }
 
-    //admin
     @PutMapping(ApiPaths.ADD_ROLE)
     public ResponseDto AddRole(@PathVariable("id") UUID userId, @RequestBody RoleDto model, @RequestHeader("Authorization") String authHeader){
         return adminService.AddRole(model, userId, authHeader.substring(7));
     }
 
-    //admin
     @DeleteMapping(ApiPaths.REMOVE_ROLE)
     public ResponseDto RemoveRole(@PathVariable("id") UUID userId, @RequestParam("roleId") UUID roleId, @RequestHeader("Authorization") String authHeader){
         return adminService.RemoveRole(userId, roleId, authHeader.substring(7));
     }
 
-    //admin
     @PutMapping(ApiPaths.BLOCK)
     public ResponseDto BlockUser(@PathVariable("id") UUID userId, @RequestHeader("Authorization") String authHeader){
         return adminService.BlockUser(userId.toString(), authHeader.substring(7));
     }
 
-    //admin
     @PutMapping(ApiPaths.UNBLOCK)
     public ResponseDto UnBlockUser(@PathVariable("id") UUID userId, @RequestHeader("Authorization") String authHeader){
         return adminService.UnBlockUser(userId.toString(), authHeader.substring(7));
